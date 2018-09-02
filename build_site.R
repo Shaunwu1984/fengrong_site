c85c89fc1e386cb6be5287b8cde6eac5ba520781
Xiaoan Wu <nantongwu@gmail.com>
2018-09-02 11:24:31 +0800

    My second website commit. The begining of greatness

--- build_site.R
+++ build_site.R
@@ -1,4 +1,13 @@
 setwd("/Users/shaun/fengrong_site")
 
+
 #render your sweet site. 
 rmarkdown::render_site()
+
+
+#cd fengrong_site/
+
+
+#git add -A
+#git commit -m "My second website commit. The begining of greatness"
+#git push origin master
